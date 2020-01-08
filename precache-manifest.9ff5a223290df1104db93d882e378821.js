2f24152e80c9e3393c07f2d433f3b11079dfaa78
Sean Mombourquette <sadfib@hotmail.com>
2020-01-08 16:17:47 -0500

    Updates

---
+++ precache-manifest.9ff5a223290df1104db93d882e378821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d57c61a3e724198c4eae0f2b358dea",
+    "url": "/monsterrolodex/index.html"
+  },
+  {
+    "revision": "81dc4204b40762dd09b2",
+    "url": "/monsterrolodex/static/css/main.082d5451.chunk.css"
+  },
+  {
+    "revision": "3154b07b8b165ad7070f",
+    "url": "/monsterrolodex/static/js/2.bf10bf7c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterrolodex/static/js/2.bf10bf7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81dc4204b40762dd09b2",
+    "url": "/monsterrolodex/static/js/main.4b598835.chunk.js"
+  },
+  {
+    "revision": "ccb329e6692140f901c7",
+    "url": "/monsterrolodex/static/js/runtime-main.eedfa8bc.js"
+  }
+]);
